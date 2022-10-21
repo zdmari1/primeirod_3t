18f68b6b887183b6d3c3c2b032dee481f7e7ec03
zdmari1 <110204951+zdmari1@users.noreply.github.com>
2022-10-21 11:59:20 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,11 +1,33 @@
+function moeda(atual) {
+  return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+
+}
 function total(){
   let c = document.getElementById("valor").value;
   let j = document.getElementById("juros").value;
   let t = document.getElementById("meses").value;
+  if (!Number(c)) {
+  alert("O capital deve ser numérico.");
+  document.getElementById("valor").value = "";
+  document.getElementById("valor").focus();
+    return
+  }
+  if (!Number(j)) {
+    alert("O juros deve ser numérico.");
+    document.getElementById("juros").value = "";
+    document.getElementById("juros").focus();
+      return
+    }
+    if (!Number(t)) {
+      alert("A quantidade de meses deve ser numérico.");
+      document.getElementById("meses").value = "";
+      document.getElementById("meses").focus();
+        return
+      }
   let r = 0;
   for(let i = 1; i <= t; i++){
       r = c * (1 + (j/100));
-      document.write("Mês " + i + " valor: " + r +"<br>");
+      document.write("Mês " + i + " valor: " + moeda(r) + ("<br>"));
       c = r;
   }
   document.write("Resultado: " + r);
