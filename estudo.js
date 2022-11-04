8808c0a6dc916e9ac7c1e0a42b9c925d5a9e22b3
zdmari1 <110204951+zdmari1@users.noreply.github.com>
2022-11-04 11:58:12 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,41 +1,50 @@
-function moeda(atual) {
-  return atual.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' });
 
-}
-function total() {
-  let c = document.getElementById("valor").value;
-  let j = document.getElementById("juros").value;
-  let t = document.getElementById("meses").value;
-  if (!Number(c)) {
-    alert("O capital deve ser numérico.");
-    document.getElementById("valor").value = "";
-    document.getElementById("valor").focus();
-    return
+function moeda(atual){
+  return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
   }
-  if (!Number(j)) {
-    alert("O juros deve ser numérico.");
-    document.getElementById("juros").value = "";
-    document.getElementById("juros").focus();
-    return
+  
+  function total(){
+    let c = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let r = moeda;
+  
+  
+    if(!Number(c)){
+        
+     alert("O capital deve ser numeros.");
+     document.getElementById("valor").value = "";
+     document.getElementById("valor").focus();
+     return;
+    }
+  
+    if(!Number(j)){   
+      alert("O juros deve ser numeros.");
+      document.getElementById("juros").value = "";
+      document.getElementById("juros").focus();
+      return;
+     }
+  
+     if(!Number(t)){
+      alert("O meses deve ser numeros.");
+      document.getElementById("meses").value = "";
+      document.getElementById("meses").focus();
+      return;
+     }
+   let mes = "";
+    for(let i = 1; i <= t; i++){
+        r = c * (1 + (j/100));
+  
+       mes += "Mês " + i + " / " + " valor: " +moeda(r) + "<br>";
+  
+        c = r;
+    }
+    document.getElementById("mes").innerHTML=mes
+  
+    document.getElementById("total").innerHTML="Total: "+moeda(r);
+    
   }
-  if (!Number(t)) {
-    alert("A quantidade de meses deve ser numérico.");
-    document.getElementById("meses").value = "";
-    document.getElementById("meses").focus();
-    return
-  }
-  let mes = ""
-  for (let i = 1; i <= t; i++) {
-    r = c * (1 + (j / 100));
-    mes += "Mês " + i + " valor: " + moeda(r) + "<br>";
-    //document.write("Mês " + i + " valor: " + moeda(r) + ("<br>"));
-    c = r;
-  }
-  document.getElementById("mes").innerHTML = mes;
-
-  document.getElementById("total").innerHTML = "Total: " + moeda(r);
-  //document.write("Resultado: " + moeda(r) );
-
+    
 
   function adicao() {
     let val1 = document.getElementById("v1").value;
